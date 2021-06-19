20e824401619f70d9e3956b8568faddf74a83902
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-18 22:40:43 -0400

    created index.php, html for signup page.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<title>MailComics</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-container w3-blue">
+  <h2>MailComics</h2>
+</div>
+
+<form class="w3-container" name="insert" action="" method="post">
+  <p>
+  <label>Email</label>
+  <input type="email" name="email" id="email" class="w3-input" type="text"></p>
+  <p>
+  <label>Password</label>
+  <input type="password" name="password" id="password" class="w3-input" type="text"></p>
+  <p>
+  <button class="w3-button w3-blue" type="submit" name="submit" value="submit">Sign Up</button>
+</form>
+
+</body>
+</html> 
