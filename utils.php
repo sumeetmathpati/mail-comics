59edd4dfdfb23bb2c7e17097905b46af9cdf434b
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-19 06:20:19 -0400

    added function to get the random iamge url from the xkcd website.

--- utils.php
+++ utils.php
@@ -220,4 +220,17 @@ function sendComicMail($email) {
     $body = $htmlMessage;
     mail($email, $subject, $body, $headers);
 }
+
+function getImgUrl() {
+    $url="https://c.xkcd.com/random/comic/";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $a = curl_exec($ch); 
+    $url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+    echo $url;
+    
+}
 ?>
