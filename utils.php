3d65f81fdb37925695810bea29327613225550a9
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-19 15:44:33 -0400

    cleaning code

--- utils.php
+++ utils.php
@@ -179,9 +179,6 @@ function sendComicMail($email, $activationcode, $imgUrl, $imgName) {
 																	style="height:auto;display:block;
 																	box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;" />
 															</p>
-															<p
-																style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">
-																Lorem ipsum.</p>
 															
 														</td>
 													</tr>
