8870872b01075c9f336d15c3250466a8f2c12356
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-19 00:39:40 -0400

    code refactoring

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
-    include_once("env.php");
+	include_once("env.php");
 
-    $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-    
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+	$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+	
+	// Check connection
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 ?>
