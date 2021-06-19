a19143b879411fdd18b5716e2cb958d66862eec4
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-19 06:31:18 -0400

    fetching image and storing to the local storage.

--- sendmail.php
+++ sendmail.php
@@ -3,8 +3,13 @@ include_once("connection.php");
 include_once("utils.php");
 
 $result = mysqli_query($con, "SELECT email, activationcode FROM users");
-$imgUrl = getImgUrl();
 
+$imgJSON = getComicJson();
+$imgUrl = $imgJSON->img;
+$filename = $imgJSON->safe_title;
+$path = '.';
+$file = $path . "/" . $filename;
+file_put_contents($file,file_get_contents($imgUrl));
 
 while($row = mysqli_fetch_array($result)) {
     sendComicMail($row['email']);
