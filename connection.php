64d815da387715b98cec687f6d03f693c48203cc
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-18 23:25:46 -0400

    refactored env.php, connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,7 @@
 <?php
     include_once("env.php");
 
-    define('DB_SERVER', getenv("HOST"));
-    define('DB_USER', getenv("USERNAME"));
-    define('DB_PASS' , getenv("PASSWORD"));
-    define('DB_NAME', getenv("DB"));
-
-    $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
     
     // Check connection
     if (mysqli_connect_errno()) {
